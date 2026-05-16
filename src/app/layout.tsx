7f1e672cede9fea4fc73f55d24eb79fa24284d6c
Induwara Ilukkumbura <157477955+induwara-iluk@users.noreply.github.com>
2026-05-16 19:22:25 +0530

    analytics applied

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import "./components.css";
 import "katex/dist/katex.min.css";
 import Navbar from "@/components/Navbar";
+import GoogleAnalytics from "@/components/GoogleAnalytics";
 
 export const metadata: Metadata = {
   title: "Physics Practicals | Explore the Laws of Nature",
@@ -14,6 +15,8 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const ga_id = process.env.NEXT_PUBLIC_GA_ID;
+
   return (
     <html lang="en">
       <head>
@@ -21,6 +24,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
       </head>
       <body>
+        {ga_id && <GoogleAnalytics ga_id={ga_id} />}
         <Navbar />
         <main>{children}</main>
       </body>
